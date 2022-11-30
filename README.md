1bacb85d7f4a64e554ba0f1b0285e53ac9f47417
Paul Funston <paulfunston9@gmail.com>
2022-11-30 12:33:02 -0600

    add media and plan functions

---
+++ README.md
@@ -0,0 +1,2 @@
+A speed typing game.
+Try and get a highscore [here](https://paul-funston.github.io/type-attack/) 
--- assets/script/index.js
+++ assets/script/index.js
@@ -9,3 +9,56 @@
 import {Score} from './Score.js'
 import { onEvent, select } from './util.js'
 
+const startBtn = select('.start');
+
+
+
+onEvent('click', startBtn, () => {
+  startGame();
+});
+
+function startGame() {
+  // if button says start change value to Try Again, else resetGame()
+  // randomize word array
+  // display gameinfo(timer points)
+  // playBGM()
+    // start timer()
+      // load word()
+    // load input()
+    // set Input focus
+}
+
+
+
+function endGame() {
+  //  displayScore(score tryAgain)
+  // stopBGM()
+
+}
+
+function resetGame() {
+  // timer to 99
+  // score to 0
+  
+}
+
+function playBGM() {
+  //start music track
+
+  // increase Tempo after 60?
+}
+
+function stopBGM() {
+  // stop music
+}
+
+function checkWord() {
+  // isInputCorrect()
+  // nextWord()
+  // clearInput
+  // playFX() 
+}
+
+function nextWord() {
+  // same as loadWord or call loadWord()?
+}
